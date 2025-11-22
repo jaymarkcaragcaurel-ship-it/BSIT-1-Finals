89575555396570bd69d3645374ae5e617fd92c89
jaymarkcaragcaurel-ship-it <jaymarkcaragcaurel@gmail.com>
2025-11-22 20:56:08 +0800

    Update ACTIVITIES.py

--- ACTIVITIES.py
+++ ACTIVITIES.py
@@ -1,6 +1,5 @@
 # WHILE LOOP LAB ACT 2
 # Sum of Even Numbers
-
 number = int(input("Enter a number: "))
 summation = 0
 number_of_run = 0
@@ -11,9 +10,10 @@ while number_of_run <= number:
     summation += number_of_run
     print(summation, "+", number_of_run)
 
+# || ========================================================================== || #
+
 # WHILE LOOP LAB ACT 2
 # Factor Finder
-
 number_to_factor = int(input("Enter a number to factor out: "))
 
 number_of_run = 0
@@ -26,11 +26,10 @@ while number_of_run <= number_to_factor:
   if factor == 0:
     print(number_of_run)
 
-
+# || ========================================================================== || #
 
 # WHILE LOOP LAB ACT 2
 # Simple ATM Menu
-
 print("ATM MENU:")
 print("1 - Deposit")
 print("2 - Withdraw")
@@ -68,56 +67,63 @@ while True:
       print("Exit")
       break
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Print 1 to 20
-
 for i in range(1, 21):
   print(i)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Even numbers
-
 for i in range(1, 41):
   if i % 2 == 0:
     print(i)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Sum of first 10 numbers
-
 summation = 0
 
 for i in range(1, 11):
   summation += i
   print(summation)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Print Characters in a Word
-
 word = str(input("Enter a word: "))
 
 for character in word:
   print(character)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Multiplication Table (1 to 10)
-
 number = int(input("Enter a number: "))
 
 for i in range(1, 11):
   print(number, "x", i, "=", number * i)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Factorial Calculator
-
 number = int(input("Enter a number to compute its factorial: "))
 
 for i in range(1, number + 1):
   if number % i == 0:
      print(i)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Count Vowels in a String
-
 string = str(input("Enter a word: "))
 summation = 0
 vowels = "aeiouAEIOU"
@@ -128,15 +134,17 @@ for vowel in string:
 
 print("There are", summation, "vowels in the string you gave")
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Reverse a word
-
 string = str(input("Enter a word: "))
 print(string[::-1])
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # Squares of a number
-
 number_to_square = int(input("How many numbers should we square? (1-50): "))
 
 print("")
@@ -144,9 +152,10 @@ print("")
 for number in range(1, number_to_square + 1):
     print(number, "when squared is:", number ** 2)
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # pattern printing
-
 rows = int(input("Enter number of rows: "))
 
 for i in range(1, rows + 1):
@@ -154,9 +163,10 @@ for i in range(1, rows + 1):
 
 print('')
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # prime number checker
-
 number = int(input("Enter a number: "))
 
 if number < 2:
@@ -169,9 +179,10 @@ else:
     else:
         print(number, "is a prime number")
 
+# || ========================================================================== || #
+
 # FOR LOOP LAB ACT 2
 # sum of digits
-
 number_input = int(input("Enter a number: "))
 string_version = str(number_input)
 sum = 0
